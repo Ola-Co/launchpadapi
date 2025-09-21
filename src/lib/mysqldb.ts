3d125e2a57681b58dec0bc4f6b8bc8cfeff770d0
shreeup <shree912@yahoo.com>
2025-09-21 13:34:38 -0700

    use env var to get db vals

--- src/lib/mysqldb.ts
+++ src/lib/mysqldb.ts
@@ -1,12 +1,22 @@
 // src/lib/mysqldb.ts
 import mysql from "mysql2/promise";
 
-export const pool = mysql.createPool({
-  host: "localhost",
-  user: "your_mysql_user",
-  password: "your_mysql_password",
-  database: "your_database",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+// export const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "your_mysql_user",
+//   password: "your_mysql_password",
+//   database: "your_database",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+import { createPool, Pool } from "mysql2/promise";
+export const pool: Pool = createPool({
+  // prefer to use .env for environment variables to hide passwords
+  host: process.env.NEXT_PUBLIC_DB_HOST || "launchpad-db.dn.odinala.io",
+  port: parseInt(process.env.NEXT_PUBLIC_DB_PORT || "8080"),
+  user: process.env.NEXT_PUBLIC_DB_USER || "app",
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD || "",
+  database: process.env.NEXT_PUBLIC_DB_DATABASE || "launchpad",
 });
